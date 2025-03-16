151b046a94eef5405877d34e94197b93f2e98040
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-03-16 19:46:33 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+      <h2 className="text-5xl blackMango font-extrabold">Aevum Beauty</h2>
+    </div>
+  );
+}
